7bd77065218ecd345f9b459ae7b8310e2b57bfa0
iNikitaGricenko <VfVristalt@gmail.com>
2023-11-03 17:24:41 +0200

    Implement Dataform CLI plugin

---
+++ src/main/java/io/kestra/plugin/dataform/cli/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Dataform CLI.\n",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.dataform.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ src/test/java/io/kestra/plugin/dataform/cli/DataformCLITest.java
@@ -0,0 +1,63 @@
+package io.kestra.plugin.dataform.cli;
+
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+class DataformCLITest {
+
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void run() throws Exception {
+        String environmentKey = "MY_KEY";
+        String environmentValue = "MY_VALUE";
+
+        DataformCLI.DataformCLIBuilder<?, ?> dataformBuilder = DataformCLI.builder()
+            .id(IdUtils.create())
+            .type(DataformCLI.class.getName())
+            .docker(DockerOptions.builder().image("dataformco/dataform:latest").entryPoint(Collections.emptyList()).build())
+            .commands(List.of("dataform --version"));
+
+        DataformCLI runner = dataformBuilder.build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, runner, Map.of("environmentKey", environmentKey, "environmentValue", environmentValue));
+
+        ScriptOutput scriptOutput = runner.run(runContext);
+        assertThat(scriptOutput.getExitCode(), is(0));
+
+        runner = dataformBuilder
+            .env(Map.of("{{ inputs.environmentKey }}", "{{ inputs.environmentValue }}"))
+            .beforeCommands(List.of(
+                "dataform init postgres new_project",
+                "cd new_project"
+            ))
+            .commands(List.of(
+                "echo \"::{\\\"outputs\\\":{" +
+                    "\\\"customEnv\\\":\\\"$" + environmentKey + "\\\"" +
+                    "}}::\"",
+                "dataform compile | tr -d ' \n' | xargs -0 -I {} echo '::{\"outputs\":{}}::'"
+            ))
+            .build();
+
+        scriptOutput = runner.run(runContext);
+        assertThat(scriptOutput.getExitCode(), is(0));
+        assertThat(scriptOutput.getVars().get("customEnv"), is(environmentValue));
+    }
+}
