7975e341c9be9b381d481123fbcd727cf5b0516e
ben8t <pimpaudben@gmail.com>
2025-12-30 15:35:21 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/dataform/cli/package-info.java
+++ src/main/java/io/kestra/plugin/dataform/cli/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "Dataform CLI",
     description = "This sub-group of plugins contains tasks for using Dataform CLI.\n",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.dataform.cli;
 
